a18a1205343c2775f1f412d05ac441b84599f59c
jakub-szewczyk <jakub.szewczyk.dev@gmail.com>
2025-04-10 20:28:08 +0200

    feat: scaffold basic sign in page

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -28,8 +28,10 @@ const handleSubmit = form.handleSubmit((values) => {
 
 <template>
   <div class="flex">
-    <div class="flex h-screen w-full flex-col items-center justify-center lg:w-1/2 xl:w-3/5">
-      <div class="absolute top-4 left-4 flex items-center gap-x-2 lg:top-6 lg:left-6">
+    <div
+      class="flex h-screen w-full flex-col items-center justify-between gap-y-4 lg:w-1/2 xl:w-3/5"
+    >
+      <div class="mt-4 ml-4 flex items-center gap-x-2 self-start lg:mt-6 lg:ml-6">
         <Compass />
         <span class="text-lg font-semibold">CareerCompass</span>
       </div>
@@ -69,13 +71,30 @@ const handleSubmit = form.handleSubmit((values) => {
                 >Forgot password?
               </RouterLink>
               <Button type="submit"> Sign in </Button>
+              <p class="text-center text-sm text-slate-500">
+                Don’t have an account?
+                <RouterLink class="text-foreground font-semibold hover:underline" to="/sign-up"
+                  >Sign up</RouterLink
+                >
+              </p>
             </form>
           </CardContent>
         </Card>
       </div>
+      <p class="px-4 pb-4 text-center text-xs text-slate-500 lg:px-6 lg:pb-6">
+        Your next opportunity is just a step away. Let's chart the path together.
+      </p>
     </div>
-    <div class="bg-primary hidden items-center justify-center lg:flex lg:w-1/2 xl:w-2/5">
+    <div
+      class="bg-primary hidden flex-col items-center justify-center gap-y-4 lg:flex lg:w-1/2 xl:w-2/5"
+    >
       <img class="w-11/12" :src="signInImage" />
+      <div class="text-center">
+        <p class="text-lg font-semibold text-white 2xl:text-xl">Track. Organize. Achieve.</p>
+        <p class="text-sm font-light text-white 2xl:text-base">
+          Stay on top of your job applications and reach your goals with ease!
+        </p>
+      </div>
     </div>
   </div>
 </template>
